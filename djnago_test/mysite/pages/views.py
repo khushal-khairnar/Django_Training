bcb3466cf47b4e32644601cae1d5679f08bf2be3
Khushal Khairnar <khushal.khairnar@neosofttech.com>
2019-04-18 14:37:57 +0530

    Django with templates and links

---
+++ djnago_test/mysite/pages/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.http import HttpResponse
+from django.views.generic import TemplateView
+
+class HomePageView(TemplateView):
+    template_name = 'home.html'
+
+class AboutPageView(TemplateView):
+    template_name = 'about.html'
+
+
+#def HomePageView(request):
+ #   return  HttpResponse('Hello World!')
